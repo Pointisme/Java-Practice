144852dc625594974029d4cba7e886691e4f636b
김정훈 <jhkim1380@naver.com>
2022-07-30 14:32:46 +0900

    정리

---
+++ Ch2/VarEx1.java
@@ -0,0 +1,12 @@
+package Ch2;
+
+class VarEx1 {
+    public static void main(String[] args)
+    {
+        int year=0;
+        int age=14;
+        System.out.println("year"+year);
+        System.out.println(age);
+
+    }
+}
