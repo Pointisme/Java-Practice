3db60b8d41ac3d6ceacd6ee1c0c199b7beac21ff
김정훈 <jhkim1380@naver.com>
2022-08-01 15:38:43 +0900

    계속공부

--- Hello.java
+++ Hello.java
@@ -1,6 +1,9 @@
 class Hello {
+    Hello today = new Hello();
     public static void main(String[] args)  //main 메소드의 선언부,,프로그램 실행할 때 'java.exe'에 의해 호출될 수 있도록 미리 약속된 부분
     {
         System.out.println("Hello, World");
+        System.out.printf("age : %d",14);
     }    
 }
+
